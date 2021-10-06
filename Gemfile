0d3b2f6c22bd7955b8e09f6f7f2cd911d838d996
charlie83xt <charlie83xt@hotmail.com>
2021-10-06 16:47:33 +0100

    Saving and open page setup + refactor

--- Gemfile
+++ Gemfile
@@ -12,3 +12,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'sinatra', '~> 2.1'
 gem 'rspec', '~> 3.10'
 gem 'capybara', '~> 3.35', '>= 3.35.3'
+gem 'launchy', '~> 2.5'
--- spec/enter_names_spec.rb
+++ spec/enter_names_spec.rb
@@ -5,6 +5,8 @@ feature 'Enters names' do
     fill_in :player_2_name, with: 'Matt'
     click_button 'Submit'
 
+    save_and_open_page
+
     expect(page).to have_content 'Charlie vs. Matt'
   end
 end
