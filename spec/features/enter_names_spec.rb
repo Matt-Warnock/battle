7f32ec12de585f5039d421d6e109b11ccba4f1db
MattW <mattnwarnock@yahoo.co.uk>
2021-10-06 17:31:45 +0100

    create web helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enters names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlie'
-    fill_in :player_2_name, with: 'Matt'
-    click_button 'Submit'
+    sign_in_and_play
 
     expect(page).to have_content 'Charlie vs. Matt'
   end
