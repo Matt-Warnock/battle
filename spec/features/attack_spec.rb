3009c55a10753c484cdfc438791acf3c79e28307
MattW <mattnwarnock@yahoo.co.uk>
2021-10-07 16:40:28 +0100

    test for HP reduction

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,4 +6,14 @@ feature 'Attacking' do
 
     expect(page).to have_content 'Charlie attacked Matt'
   end
+
+  scenario 'reduce player 2 HP by 10' do
+    sign_in_and_play
+
+    click_link 'Attack'
+    click_link 'OK'
+
+    expect(page).not_to have_content('Matt: 60HP')
+    expect(page).to have_content('Matt: 50HP')
+  end
 end
